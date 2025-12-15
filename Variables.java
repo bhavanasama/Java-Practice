5d83789976b46c28dfdeaa0fefaea85a088ad247
bhavanasama <136094199+bhavanasama@users.noreply.github.com>
2025-12-15 22:21:32 +0530

    Primitive Data Types

---
+++ Variables.java
@@ -0,0 +1,21 @@
+public class Variables{
+    public static void main(String[] args){
+        int myNum = 5;               // Integer (whole number)
+        float myFloatNum = 5.99f;    // Floating point number
+        char myLetter = 'D';         // Character
+        boolean myBool = true;       // Boolean
+        String myText = "Hello";     // String
+        long myLongNum = 15000000000L; // Long integer
+        double myDoubleNum = 19.99d; // Double precision floating point number
+        byte myByte = 100;          // Byte
+
+        System.out.println("Integer: " + myNum);
+        System.out.println("Float: " + myFloatNum);
+        System.out.println("Character: " + myLetter);
+        System.out.println("Boolean: " + myBool);
+        System.out.println("String: " + myText);
+        System.out.println("Long: " + myLongNum);
+        System.out.println("Double: " + myDoubleNum);
+        System.out.println("Byte: " + myByte);
+    }
+}
