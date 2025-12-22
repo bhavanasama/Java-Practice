fb63b71c9ced3fed51b2fd68296c9e4261442a69
bhavanasama <136094199+bhavanasama@users.noreply.github.com>
2025-12-22 23:13:36 +0530

    Types of Comments

---
+++ Comments.java
@@ -0,0 +1,18 @@
+public class Comments {
+
+    /** (documentation comment)
+     * Main method
+     * @param args  Command line arguments
+     * returns void
+     */
+    public static void main(String[] args) {
+        // This is a single-line comment
+
+        /*
+         This is a multi-line comment.
+         It can span multiple lines.
+        */
+
+        System.out.println("Comments in Java!");
+    }
+}
