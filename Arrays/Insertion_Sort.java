f02ac6a05a70f5395d4e747cc938c52976ddb22a
bhavanasama <136094199+bhavanasama@users.noreply.github.com>
2026-02-04 22:57:56 +0530

    Insertion Sort

---
+++ Arrays/Insertion_Sort.java
@@ -0,0 +1,36 @@
+public class Insertion_Sort {
+    /**
+     * Sorts the array in ascending order using insertion sort.
+     */
+    public static void insertionSort(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            int key = arr[i];
+            int j = i - 1;
+            // Move elements of arr[0..i-1] that are greater than key
+            // to one position ahead of their current position
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = key;
+        }
+    }
+
+    public static void printArray(int[] arr) {
+        for (int v : arr) {
+            System.out.print(v + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 12, 11, 13, 5, 6 };
+        System.out.print("Before: ");
+        printArray(arr);
+
+        insertionSort(arr);
+
+        System.out.print("After:  ");
+        printArray(arr);
+    }
+}
