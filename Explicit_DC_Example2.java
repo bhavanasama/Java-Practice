e4bd9ce116766b74d330db63a8c822d23b203628
bhavanasama <136094199+bhavanasama@users.noreply.github.com>
2026-02-13 23:26:42 +0530

    Explicit Default Constructor

---
+++ Explicit_DC_Example2.java
@@ -0,0 +1,20 @@
+//Explicit Default Constructor is said as Non-Parameterized Constructor. 
+// It is a constructor which does not have any parameters. It is used to initialize the instance variables with default values. 
+// If we do not provide any constructor in the class, then the Java compiler automatically provides a default constructor. 
+// However, if we provide any constructor (parameterized or non-parameterized), then the Java compiler does not provide a default constructor.
+
+public class Explicit_DC_Example2 {
+    int x;
+    String y;
+    Explicit_DC_Example2() {
+        System.out.println("This is an explicit default constructor.");
+    }
+    void display() {
+        System.out.println("Value of x: " + x);
+        System.out.println("Value of y: " + y);
+    }
+    public static void main(String[] args) {
+        Explicit_DC_Example2 obj = new Explicit_DC_Example2();
+        obj.display();
+    }
+}
